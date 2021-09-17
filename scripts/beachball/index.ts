12c39c3c6b8c9478e2ce3331ff250f23c9164bc6
Lingfan Gao <lingfangao@hotmail.com>
2021-09-17 10:12:41 +0200

    update beachball for private registry

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -1,6 +1,8 @@
 import { BeachballConfig } from 'beachball';
 import { getAllPackageInfo } from '../monorepo/index';
-import { renderHeader, renderEntry } from './customRenderers';
+import { writeFile } from 'fs-extra';
+const path = require('path');
+// import { renderHeader, renderEntry } from './customRenderers';
 
 const allPackageInfo = getAllPackageInfo();
 const fluentConvergedPackagePaths = Object.values(allPackageInfo)
@@ -9,11 +11,9 @@ const fluentConvergedPackagePaths = Object.values(allPackageInfo)
       return packageInfo.packagePath;
     }
 
-    // These packages depend on converged packages, but are private
-    // Can be included in the publish scope so that dependencies are bumped correctly.
-    const privateNonConverged = ['perf-test', 'vr-tests'];
+    const hardCoded = ['perf-test', 'vr-tests'];
 
-    if (privateNonConverged.includes(packageInfo.packageJson.name)) {
+    if (hardCoded.includes(packageInfo.packageJson.name)) {
       return packageInfo.packagePath;
     }
 
@@ -23,22 +23,43 @@ const fluentConvergedPackagePaths = Object.values(allPackageInfo)
 
 const ignoreFluentConvergedScope = fluentConvergedPackagePaths.map(path => `!${path}`);
 // Northstar is never published with beachball
-const ignoreNorthstarScope = '!packages/fluentui/*';
+const northstarScope = '!packages/fluentui/*';
 
-// Default scope used to publish Fluent UI v8 and non-v9 packages
-const defaultScope = [ignoreNorthstarScope, ...ignoreFluentConvergedScope];
+// Default scope used to publish Fluent UI v8
+const defaultScope = [northstarScope, ...ignoreFluentConvergedScope];
 
 export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
   tag: 'latest',
   generateChangelog: true,
+  gitTags: false,
+  registry:
+    'https://uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/',
   // @ts-ignore
   scope: process.env.RELEASE_VNEXT ? fluentConvergedPackagePaths : defaultScope,
-  changelog: {
-    customRenderers: {
-      renderHeader,
-      renderEntry,
+  hooks: {
+    prepublish: packagePath => {
+      const authToken = process.env.ADO_TOKEN;
+      return writeFile(
+        path.join(packagePath, '.npmrc'),
+        `
+; begin auth token
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:email=npm requires email to be set but doesn't use the value
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+      `,
+      );
     },
+  },
+  changelog: {
+    // customRenderers: {
+    //   renderHeader,
+    //   renderEntry,
+    // },
     groups: [
       {
         masterPackageName: '@fluentui/react-components',
